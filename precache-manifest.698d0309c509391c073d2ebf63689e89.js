01a87b04b71caa972fe33ac3596f57baa9836621
Ronak Patel <rpatel2121@ucla.edu>
2019-02-25 23:22:30 -0800

    Updates

---
+++ precache-manifest.698d0309c509391c073d2ebf63689e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32ec863f15a18c42d0e1",
+    "url": "/evzone/static/css/main.b846ece8.chunk.css"
+  },
+  {
+    "revision": "32ec863f15a18c42d0e1",
+    "url": "/evzone/static/js/main.32ec863f.chunk.js"
+  },
+  {
+    "revision": "0c5e29a107261c037151",
+    "url": "/evzone/static/js/runtime~main.0c5e29a1.js"
+  },
+  {
+    "revision": "c1e0b7efa5b942186198",
+    "url": "/evzone/static/js/2.c1e0b7ef.chunk.js"
+  },
+  {
+    "revision": "84805a96e858fe630fa27a5d2c7508f9",
+    "url": "/evzone/static/media/logo.84805a96.png"
+  },
+  {
+    "revision": "429ea3ced5ec7fd82c7f9c93307336c9",
+    "url": "/evzone/index.html"
+  }
+];
